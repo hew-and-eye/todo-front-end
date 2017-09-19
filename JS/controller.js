3c1ab58549803e3780962194d84c7581d0b06929
hew-and-eye <matthew.r.bernardo@gmail.com>
2017-09-19 15:55:34 -0400

    made changes to task layout, added http calls, and implemented some client side editing. Still need to send changes to the servlet.

--- JS/controller.js
+++ JS/controller.js
@@ -1,20 +1,51 @@
-app.controller('todoController', function($scope, $location) {
+app.controller('todoController', function($scope, $location, $http) {
     // code for authentication //
-    console.log("in controller");
     $scope.username;
     $scope.password;
-    $scope.loggedIn;
+    $scope.loggedIn = true;
     $scope.confirmPassword = "";
     $scope.newUser = false;
+    $scope.currentTask = 0;
+    $scope.assignUsers = false;
     $location.path("/");
     // to submit, check that fields are filled, and submit to server if they are
-    $scope.login = function() {
-        console.log("blorp");
-        if($scope.username != "" && $scope.password != "" && (!$scope.newUser || $scope.password == $scope.confirmPassword))
-        {
-            console.log("changing path");
-            $location.path("/manage")
+    $scope.login = function () {
+        $scope.username = document.getElementById("username").value;
+        $scope.password = document.getElementById("password").value;
+        console.log($scope.username);
+        if ($scope.username != "" && $scope.password != "" && (!$scope.newUser || $scope.password == $scope.confirmPassword)) {
+            $http({
+                method: 'POST',
+                dataType: "application/json",
+                url: 'http://localhost:8080/TodoWebApp/TodoServlet',
+                headers: {
+                    //'Content-Type': 'application/x-www-form-urlencoded'
+                    'Content-Type': 'application/json'
+                },
+                contentType: 'application/json',
+                mimeType: 'application/json',
+                params: {
+                    "optype": "login",
+                    "username": $scope.username,
+                    "password": $scope.password
+                }
+            }).then(function successCallback(response) {
+                if (response.data.error == "false") {
+                    $scope.tasks = [{"name":"", "description": "", "users":[]}].concat(response.data.data);
+                    $location.path("/manage");
+                }
+                else alert(response.data.data);
+                console.log(response);
+                // this callback will be called asynchronously
+                // when the response is available
+            }, function errorCallback(response) {
+                console.log(response);
+                alert("couldn't reach the server for verification :(");
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
         }
+        else alert("missing input fields");
     }
     // on server callback, route to view page
     /////////////////////////////
@@ -26,13 +57,16 @@ app.controller('todoController', function($scope, $location) {
         {"name":"Eat groceries", "description":"And by groceries, I mean instant ramen, usually", "complete":"false",
             "users":["pleb", "clorb", "glox", "ranzamatab"]},
         {"name":"Buy groceries", "description":"let's be honest, I just want more snacks", "complete":"false",
-            "users":["pleb", "clorb", "glox", "ranzamatab"]}]; // array of JSONs with task data
+            "users": ["pleb", "clorb", "glox", "ranzamatab"]
+        }]; // array of JSONs with task data
+
+    $scope.users = ["pleb", "clorb", "glox", "ranzamatab", "gromcrob", "chilbus", "gezgoz", "dormulan", "tramford", "riggis", "nalbuton"];
+    $scope.userSearchText = "";
     //////////////////////////////////////
 
     // stuff for making/editing a task //
-    $scope.taskName;
-    $scope.taskDesc;
-    $scope.taskDone;
+    $scope.newTaskName;
+    $scope.newTaskDesc;
     $scope.taskId;
     $scope.assignedUsers;
     $scope.delete = function() {
@@ -53,7 +87,7 @@ app.controller('todoController', function($scope, $location) {
             console.log("added expanded variable to task");
         }
         $scope.tasks[index].expanded = !$scope.tasks[index].expanded;
-        let height = "8vh";
+        let height = "10vh";
         if($scope.tasks[index].expanded) height = "100vh";
         document.getElementById("tc-"+index).style.maxHeight=height;
         // $scope.collapse($scope.prevTask);
@@ -68,4 +102,15 @@ app.controller('todoController', function($scope, $location) {
     }
 
     /////////////////////////////////////
+    $scope.editUsers = function (index) {
+        let oldUsers = $scope.tasks[index].users;
+        $scope.oldUsers = oldUsers;
+        $scope.assignUsers = true;
+        $scope.currentTask = index;
+    }
+    $scope.closeAssign = function (keepChanges) {
+        if (!keepChanges)
+            $scope.tasks[$scope.currentTask].users = $scope.oldUsers;
+        $scope.assignUsers = false;
+    }
 });
